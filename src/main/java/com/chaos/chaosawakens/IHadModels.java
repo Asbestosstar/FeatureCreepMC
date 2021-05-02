4b2cd36813d4573a9bd617ba3a66dc0340b77ae3
Asbestosstar <ricsch97@gmail.com>
2021-05-02 02:16:12 -0700

    Add files via upload

---
+++ src/main/java/com/chaos/chaosawakens/IHadModels.java
@@ -0,0 +1,5 @@
+package com.chaos.chaosawakens;
+
+public interface IHadModels {
+public void registerModels();
+}
---
+++ src/main/java/com/chaos/chaosawakens/IMetaName.java
@@ -0,0 +1,7 @@
+package com.chaos.chaosawakens;
+
+import net.minecraft.item.ItemStack;
+
+public interface IMetaName {
+public String getSpecialName(ItemStack stack);
+}
