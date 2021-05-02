3e56cfc5bf53d0b956d35e13ccb90843672ae20d
Asbestosstar <ricsch97@gmail.com>
2021-05-02 01:56:02 -0700

    Add files via upload

---
+++ src/minecraft/featurecreep/tools/amethyst/AmethystHoe.java
@@ -0,0 +1,21 @@
+package featurecreep.tools.amethyst;
+
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.EnumToolMaterial;
+import net.minecraft.item.ItemHoe;
+
+public class AmethystHoe extends ItemHoe{
+
+	public AmethystHoe(int par1, EnumToolMaterial par2EnumToolMaterial) {
+		super(par1, par2EnumToolMaterial);
+		// TODO Auto-generated constructor stub
+		this.setCreativeTab(CreativeTabs.tabTools);
+		
+	}
+
+	public void registerIcons(IconRegister iconRegister)
+	{
+	itemIcon = iconRegister.registerIcon("featurecreep:amethyst_hoe");	
+	}
+}
---
+++ src/minecraft/featurecreep/tools/generic/GenericAxe.java
@@ -0,0 +1,21 @@
+package featurecreep.tools.generic;
+
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.EnumToolMaterial;
+import net.minecraft.item.ItemAxe;
+
+public class GenericAxe extends ItemAxe{
+
+	protected GenericAxe(int par1, EnumToolMaterial par2EnumToolMaterial) {
+		super(par1, par2EnumToolMaterial);
+		// TODO Auto-generated constructor stub
+		this.setCreativeTab(CreativeTabs.tabTools);
+		
+	}
+
+	public void registerIcons(IconRegister iconRegister)
+	{
+	itemIcon = iconRegister.registerIcon("featurecreep:" + this.getUnlocalizedName());	
+	}
+}
