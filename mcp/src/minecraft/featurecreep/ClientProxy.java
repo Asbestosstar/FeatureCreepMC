7c8ea89cbbbb4bedba2aa2684355ebd2c9d758a3
Asbestosstar <ricsch97@gmail.com>
2021-05-23 11:52:54 -0700

    A beta Build for 2,0ESR

---
+++ mcp/src/minecraft/featurecreep/ClientProxy.java
@@ -0,0 +1,13 @@
+package featurecreep;
+import featurecreep.FeatureCreepMC;
+
+
+public class ClientProxy extends CommonProxy{
+
+	public void registerRenderInformation()
+	{
+		//nothing
+	}
+	
+	
+}
---
+++ mcp/src/minecraft/featurecreep/CommonProxy.java
@@ -0,0 +1,12 @@
+package featurecreep;
+import featurecreep.FeatureCreepMC;
+
+public class CommonProxy {
+
+	public void registerRenderInformation()
+	{
+		//Nothing
+	}
+	
+	
+}
