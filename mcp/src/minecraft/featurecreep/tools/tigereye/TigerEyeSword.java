3dc78ebfe2cc79c1beffa3b87048dfbb220d679f
Asbestosstar <ricsch97@gmail.com>
2021-05-23 11:54:21 -0700

    Add files via upload

---
+++ mcp/src/minecraft/featurecreep/tools/tigereye/TigerEyeSword.java
@@ -0,0 +1,21 @@
+package featurecreep.tools.tigereye;
+
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.EnumToolMaterial;
+import net.minecraft.item.ItemSword;
+
+public class TigerEyeSword extends ItemSword{
+
+	public TigerEyeSword(int par1, EnumToolMaterial par2EnumToolMaterial) {
+		super(par1, par2EnumToolMaterial);
+		// TODO Auto-generated constructor stub
+		this.setCreativeTab(CreativeTabs.tabCombat);
+		
+	}
+
+	public void registerIcons(IconRegister iconRegister)
+	{
+	itemIcon = iconRegister.registerIcon("featurecreep:tigers_eye_sword");	
+	}
+}
